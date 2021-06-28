764c6159c6300cf8f233eadd1ebb2f77bd03b166
amaykumar1910 <76843826+amaykumar1910@users.noreply.github.com>
2021-06-28 21:33:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+var b1, b2;
+
+function setup() {
+  createCanvas(400, 400);
+  b1=new Building();
+  b1.position=3;
+  b1.floors=22;
+  
+  b2=new Building();
+  b2.position=1;
+  b2.floors=20;
+  
+  b3=new Building();
+  b3.position=2;
+  b3.floors=11;
+  
+  b4=new Building();
+  b4.position=4;
+  b4.floors=13;
+  
+  b5=new Building();
+  b5.position=5;
+  b5.floors=24;
+  
+  b6=new Building();
+  b6.position=6;
+  b6.floors=15;
+
+  b7=new Building();
+  b7.position=7;
+  b7.floors=21;
+  
+  b8=new Building();
+  b8.position=8;
+  b8.floors=17;
+
+  b9=new Building();
+  b9.position=9;
+  b9.floors=18;
+  
+   b10=new Building();
+  b10.position=10;
+  b10.floors=19;
+  
+    
+}
+
+function draw() {
+  background(0);
+  b1.display();
+  b2.display();
+  b3.display();
+  b4.display();
+  b5.display();
+  b6.display();
+  b7.display();
+  b8.display();
+  b9.display();
+  b10.display();
+  
+}
